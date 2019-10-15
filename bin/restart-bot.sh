3c99a2439409490026e4c7b9f4be9a7ae3594474
Thomas Bindzus <bindzus@gmail.com>
2019-10-15 14:07:46 +0700

    Final script for restarting botkit process

--- bin/restart-bot.sh
+++ bin/restart-bot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo 'Calling PM2 to restart the bot';
 
-if [ -x pm2 ]; then
+pm2 = `which pm2`
+
+if [[ -x $pm2 ]]; then
   echo 'PM2 found'
+  pm2 restart botkit
 else
   echo 'PM2 not found'
 fi
